387099a68e6a4f126cc9b338b71b93935b711c52
V-Mary <74894916+V-Mary@users.noreply.github.com>
2021-10-27 19:38:57 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,33 +1,38 @@
-import {
-    getInputValues,
-    clearInputs
-  } from "./dom.js";
 
-import {
-    postChainsaw,
-    getAllChainsaws
-} from "./api.js";
-const submitButton = document.getElementById("submit_button");
+const searchButton = document.getElementById("search_button");
+const sorthButton = document.getElementById("sort_button");
+
+const itemsContainer = document.getElementById("items_container");
 
 
-export const refetchAllChainsaws = async () => {
-    const allChainsaws = await getAllChainsaws();
-  
-    chainsaws = allChainsaws;
-  
-    renderItemsList(chainsaws, onEditItem, onRemoveItem);
-  };
+const itemTemplate = ({ id, name, power, price }) => `
+<li id="${id}" class="item-card">
+  <div>
+    <h5>${name}</h5>
+    <p>${power}</p>
+    <p>Price: ${price}</p>
+  </div>
+</li>`;
 
-submitButton.addEventListener("click", (event) => {
-    event.preventDefault();
-  
-    const { name, power, price } = getInputValues();
-  
-    clearInputs();
-  
-    postChainsaw({
-      name,
-      power,
-      price
-    }).then(refetchAllChainsaws);
-  });
+
+let chainsaws = [
+  { id: 1, name: 'as', power: 2, price: 1200},
+  { id: 2, name: 'nh', power: 4, price: 1600},
+  { id: 3, name: 'od', power: 5, price: 120},
+];
+
+const addItemToPage = ({ _id: id, name, power, price }) => {
+  itemsContainer.insertAdjacentHTML(
+    "afterbegin",
+    itemTemplate({ id, name, power, price })
+  );  
+};
+
+const renderItemsList = (items, onEditItem, onRemoveItem) => {
+  itemsContainer.innerHTML = "";
+
+  for (const item of items) {
+    addItemToPage(item, onEditItem, onRemoveItem);
+  }
+};
+
